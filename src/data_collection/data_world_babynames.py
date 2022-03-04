34449aa1fcc424d5250468ab3f58da4d7716d697
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2022-03-04 15:27:51 +1100

    Add dataworld baby names

---
+++ src/data_collection/data_world_babynames.py
@@ -0,0 +1,26 @@
+"""Data World - Baby Names.
+
+Source:
+    - https://data.world/alexandra/baby-names
+"""
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+import utils as ut  # noqa
+
+url = "https://query.data.world/s/pyywmosekc5zst7l5imicuuyltsumv"
+names_df = pd.read_csv(url, header=None, names=["first_name", "gender"])
+
+
+# Restructure into the required format
+names_df["gender"] = names_df["gender"].apply(ut.remap_gender)
+names_df["origin"] = pd.NA
+names_df["definition"] = pd.NA
+
+
+# Save
+names_df.to_csv("data/data_world_babynames.csv", sep="|", index=False, encoding="utf-8")
